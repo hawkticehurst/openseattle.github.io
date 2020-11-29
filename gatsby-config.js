a07bdd0d9ce4b7370fa899b38d154d1c2e8be6a8
Hawk Ticehurst <hawkticehurst@gmail.com>
2020-11-28 21:36:57 -0800

    Change website icon in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         background_color: "#fff",
         theme_color: "#fff",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        icon: "src/images/openseattle-icon.png", // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
