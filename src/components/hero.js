e7efb65eb11865f3852d85293e33eb775deafb7a
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-01-08 23:35:53 -0800

    Update hero button links

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,8 +7,8 @@ const Hero = ({ heroText }) => (
   <section className="hero-container">
     <h1 className="hero-text">{heroText}</h1>
     <section className="hero-btn-container">
-      <PrimaryButton buttonText="I'm a volunteer" link="/about" />
-      <PrimaryButton buttonText="I'm an organization" link="/about" />
+      <PrimaryButton buttonText="I'm a volunteer" link="/" />
+      <PrimaryButton buttonText="I'm an organization" link="/" />
     </section>
   </section>
 );
